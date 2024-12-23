5dc6e55acd7c0b9af4351c3c883b5759cee6a074
Yaroslav Yakupov <yakupovyaroslav@icloud.com>
2024-12-23 22:09:57 +0300

    TEST

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { lazy, Suspense } from 'react';
 import './App.css'
 import HostAppComponent from './components/hostAppComponent'
+
+const SecondAppComponent = lazy(() => import('sharedComponents/SecondAppComponent') as any);
 
 function App() {
   return (
     <>
       <h1>Host app</h1>
       <HostAppComponent />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SecondAppComponent/>
+      </Suspense>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  preview: {
+    host: 'localhost',
+    port: 5000,
+    strictPort: true,
+  },
+  server: {
+    port: 5000,
+  },
+  plugins: [
+    react(),
+    federation({
+      name: 'vite-federation-test-one',
+      remotes: {
+        sharedComponents: "http://localhost:5001/assets/remoteEntry.js",
+      },
+      shared: ['react', 'react-dom']
+    })
+  ],
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
 })
